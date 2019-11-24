129d7c430cc7e20fa62900e5e2da9ef2c52d2691
Skylar Cupit <Skylarcupit@gmail.com>
2019-11-24 04:17:31 -0600

    Added basic README, config modifications

---
+++ README.md
@@ -0,0 +1,6 @@
+# JSON in C++
+The purpose of this library is to make using JSON in C++ simple.
+A dynamic Json Value object is provided, as well as a parser which uses the Json Value.
+
+## How to Build
+Using CMake, generate an out of source build. The Test executable, example executable, and shared/static libraries will be built.
--- main.cpp
+++ main.cpp
@@ -1,45 +1,40 @@
 #include <iostream>
 #include "JsonParser.hpp"
 #include "JsonValue.hpp"
-#include <fstream>
 
 using namespace std;
 
 int main(void) {
 
-  // JsonValue jVal({
-  //   {"hello", "Noice thing"},
-  //   {"Very nice plan", "other place"},
-  //   {"An Array", JsonValue(
-  //     {
-  //       12,
-  //       -32.736f,
-  //       "someone",
-  //       nullptr,
-  //       false,
-  //       true,
-  //       false,
-  //       JsonValue({
-  //         {"This object is in the array", nullptr},
-  //         {"Noice", -10}
-  //       })
-  //     }
-  //   )},
-  //   {"Another Object", JsonValue(
-  //     {
-  //       {"Key in that object", "very nice"},
-  //       {"You", 12.3f},
-  //       {"boolean", true}
-  //     }
-  //   )},
-  //   {"Should be null", nullptr}
-  // });
+  JsonValue jVal({
+    {"hello", "Noice thing"},
+    {"Very nice plan", "other place"},
+    {"An Array", JsonValue(
+      {
+        12,
+        -32.736f,
+        "someone",
+        nullptr,
+        false,
+        true,
+        false,
+        JsonValue({
+          {"This object is in the array", nullptr},
+          {"Noice", -10}
+        })
+      }
+    )},
+    {"Another Object", JsonValue(
+      {
+        {"Key in that object", "very nice"},
+        {"You", 12.3f},
+        {"boolean", true}
+      }
+    )},
+    {"Should be null", nullptr}
+  });
 
-  JsonParser jParser("D:\\Personal_Projects\\Coding\\projects\\c++\\json_parser_2\\cmake_data.json");
-  // ofstream fout("D:\\Personal_Projects\\Coding\\projects\\c++\\json_parser_2\\temp_json\\ToFile.json");
-  // fout << jParser.getJsonReference();
-  // fout.close();
+  cout << jVal << endl;
 
-  cout << jParser.getJsonReference() << endl;
   return 0;
 }
